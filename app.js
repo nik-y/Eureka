d45d6433916a1addb4b8a71264c6db7c97504c07
Nikhil Yadav <bestnikhilyadav@gmail.com>
2021-02-16 11:13:22 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var campgroundRoutes = require('./routes/campgrounds'),
 	indexRoutes      = require('./routes/index');
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://NikYMLab:elgoog123@cluster0.z3o8p.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
